a741ade03c3b6f29d7a949bf284dd1630136a70e
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-27 14:31:37 -0500

    Auto-commit: Thu Nov 27 14:31:37 2025

--- SIMULATION/build_sim.sh
+++ SIMULATION/build_sim.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Compile with debugging symbols
+if [ ! -d "../utils/Raylib" ]; then
+  echo "Raylib not found. Installing..."
+  git clone https://github.com/raysan5/raylib.git ../utils/Raylib/
+else
+  echo "Raylib found. Skipping installation."
+fi
+
 gcc -w -o sim sim.c ../utils/Raylib/raylib.c ../utils/NN/NN.c ../utils/NN/NEAT.c 
 
 if [ $? -eq 0 ]; then
