523a6306ce1b593233d33e2b413f6191831a87a4
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-28 18:56:47 -0500

    Auto-commit: Fri Nov 28 18:56:47 2025

--- GAME/main.c
+++ GAME/main.c
@@ -2,9 +2,10 @@
 
 #ifdef STATUS(void*fn(int argc, char **argv)) 
   int res = assert(fn(argc, argv) == 0);
-  #define ERROR 0
-#else
-  #define SUCCESS 1
+  #if res
+    #define SUCCESS 1
+  #else
+    #define ERROR 0
 #endif
 
 void hello(int argc, char **argv) {
