9ecd29bf1b52b5c203694ddf613b40f8e74f7aa7
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-28 18:59:18 -0500

    Auto-commit: Fri Nov 28 18:59:18 2025

--- GAME/main.c
+++ GAME/main.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
 
-#define STATUS(fn_call, status_var)      \
-    do {                                 \
-        int _r = (fn_call);              \
-        if (_r == 0)                     \
-            (status_var) = 1;            \
-        else                             \
-            (status_var) = 0;            \
-    } while (0)
+int main() {
 
-void hello() {
-    printf("hello world\n");
-}
-
-int main(void) {
-    int status;
-
-    STATUS(hello(0, NULL), status);
-
-    printf("status = %d\n", status); // prints 1 if success
-
-    return status;
+  printf("Hello, World!");
+  return 0;
 }
 
