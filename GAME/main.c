1563c40ba82bebf4dd3f4f09c9805cb5c5f727c5
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-28 18:58:37 -0500

    Auto-commit: Fri Nov 28 18:58:37 2025

--- GAME/main.c
+++ GAME/main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-#ifdef STATUS(void*fn(int argc, char **argv)) 
-  int res = assert(fn(argc, argv) == 0);
-  #if res
-    #define SUCCESS 1
-  #else
-    #define ERROR 0
-  #endif
-#endif
+#define STATUS(fn_call, status_var)      \
+    do {                                 \
+        int _r = (fn_call);              \
+        if (_r == 0)                     \
+            (status_var) = 1;            \
+        else                             \
+            (status_var) = 0;            \
+    } while (0)
 
-void hello(int argc, char **argv) {
-  printf("hello world\n");
+int hello(int argc, char **argv) {
+    printf("hello world\n");
+    return 0;   // success
 }
 
 int main(void) {
-  int status; 
-  STATUS(hello(0, NULL));
+    int status;
 
-  return status;
+    STATUS(hello(0, NULL), status);
+
+    printf("status = %d\n", status); // prints 1 if success
+
+    return status;
 }
 
