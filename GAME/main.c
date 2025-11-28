e2a36909630be5cd16fc8c851055e36f734b60b7
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-28 18:46:10 -0500

    Auto-commit: Fri Nov 28 18:46:10 2025

--- GAME/main.c
+++ GAME/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+#define STATUS _Generic(NULL, \
+  int: "%d", \
+  char: "%c", \
+  float: "%f", \
+  double: "%f", \
+  default: "%p")
+
 int main(void) {
   printf("hello world\n");
 
