a10c1d07640a28684a0385f934233d06b71a6466
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-28 19:06:50 -0500

    Auto-commit: Fri Nov 28 19:06:50 2025

--- GAME/Env.h
+++ GAME/Env.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../utils/Raylib/src/raylib.h"
-#include "../utils/NN/*
+#include "../utils/NN/*.h"
 typedef struct {
   enum EntityType {
     PLAYER,
