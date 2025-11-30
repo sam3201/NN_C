59eecfc02fbd1cf278abec912dd425022b093406
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2025-11-30 18:09:11 -0500

    Auto-commit: Sun Nov 30 18:09:11 2025

--- HUGGINGFACE_INTEGRATION/run.sh
+++ HUGGINGFACE_INTEGRATION/run.sh
@@ -1,6 +1,6 @@
-python3 hf_trainer.py bert-base-uncased 5 vocabulary
+python3 hf_trainer.py bert-base-uncased 1 vocabulary
 
-./hf_trainer bert-base-uncased 5 hf_training_data.json
+./hf_trainer bert-base-uncased 1 hf_training_data.json
 
 ./sam_hf_bridge bert-base-uncased interactive
 
