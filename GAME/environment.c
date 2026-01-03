ae79fc598cc9a78ed06e8c751ff00764e1a4e389
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 13:09:19 -0500

    Auto-save

--- GAME/environment.c
+++ GAME/environment.c
@@ -69,6 +69,9 @@ void generate_chunk(Chunk *c, int cx, int cy) {
     c->resources[i].position =
         (Vector2){(float)(rand() % CHUNK_SIZE), (float)(rand() % CHUNK_SIZE)};
     c->resources[i].type = rand() % RES_COUNT;
+    c->resources[i].health = 100;
+    c->resources[i].alive = true;
+
     c->resources[i].visited = false;
   }
 
