2df0fbffbe26780c0f42c3dc8e85797687d92a86
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 22:11:09 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -132,7 +132,7 @@ int ai_choose_action(Tank *ai, Tank *player) {
                        .dirichlet_eps = 0.25f,
                        .temperature = 1.0f,
                        .discount = 0.99f};
-  MCTSResult *res = mcts_run(ai_model, obs, &params);
+  MCTSResult res = mcts_run(ai_model, obs, &params);
   int action = res->chosen_action;
   mcts_result_free(res);
   return action;
