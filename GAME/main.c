89137f3bdd94b28bd311d54d4912c2cb6b8c1d7c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 21:07:16 -0500

    Auto-save

--- GAME/main.c
+++ GAME/main.c
@@ -3,9 +3,22 @@
 #include "../utils/Raylib/src/raylib.h"
 
 typedef struct {
+  int x;
+  int y;
+  int width;
+  int height;
+  int health;
+  int maxHealth;
+  int speed;
+  int damage;
+  int direction;
+  int xVelocity;
+  int yVelocity;
 
-  int main(void) {
-    printf("Hello, World!");
+} Tank;
 
-    return 0;
-  }
+int main(void) {
+  printf("Hello, World!");
+
+  return 0;
+}
