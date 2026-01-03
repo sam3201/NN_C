de67c4117634b26c67b97a3c909b70735a2a3d5f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 18:56:11 -0500

    Auto-save

--- utils/NN/MUZE/runtime.h
+++ utils/NN/MUZE/runtime.h
@@ -6,15 +6,23 @@
 
 typedef struct {
   ReplayBuffer *rb;
-
   float *last_obs;
   int last_action;
   int has_last;
-
   float gamma;
 } MuRuntime;
 
+/* Runtime lifecycle */
 MuRuntime *mu_runtime_create(MuModel *model, int capacity, float gamma);
 void mu_runtime_free(MuRuntime *rt);
+
+/* Internal runtime ops */
+void mu_runtime_step(MuRuntime *rt, MuModel *model, const float *obs,
+                     int action, float reward);
+
+void mu_runtime_end_episode(MuRuntime *rt, MuModel *model,
+                            float terminal_reward);
+void mu_runtime_reset_episode(MuRuntime *rt);
+void mu_runtime_train(MuRuntime *rt, MuModel *model);
 
 #endif
