2365c7f94696f63ceacce78eca43e21faaa7567f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 13:13:27 -0500

    Auto-save

--- GAME/environment.h
+++ GAME/environment.h
@@ -80,8 +80,38 @@ void update_player(void);
 void draw_player(Vector2 camera);
 void draw_ui(void);
 
+void spawn_resource(Vector2 pos, ResourceType type) {
+  if (resource_count >= MAX_RESOURCES)
+    return;
+
+  Resource *r = &resources[resource_count++];
+  r->position = pos;
+  r->type = type;
+  r->health = (type == RES_TREE) ? 5 : 8;
+  r->visited = false;
+}
+
+void draw_resources(Vector2 camera) {
+  for (int i = 0; i < resource_count; i++) {
+    Resource *r = &resources[i];
+    if (!r->health <= 0)
+      continue;
+
+    Vector2 s = {r->position.x - camera.x, r->position.y - camera.y};
+
+    if (r->type == RES_TREE) {
+      DrawRectangle(s.x - 4, s.y - 12, 8, 16, DARKGREEN);
+      DrawCircle(s.x, s.y - 16, 10, GREEN);
+    } else {
+      DrawCircle(s.x, s.y, 8, GRAY);
+    }
+  }
+}
+
 extern Player player;
 extern Resource resources[MAX_RESOURCES];
 extern int resource_count = 0;
+
+Resource *find_nearest_resource(Vector2 pos, float max_dist);
 
 #endif
