3e206ae6d995cbd82118ffae0cc1e1d771787b18
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 18:21:34 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -330,7 +330,8 @@ int main() {
   SCREEN_WIDTH = GetScreenWidth();
   SCREEN_HEIGHT = GetScreenHeight();
 
-  TILE_SIZE = (float)SCREEN_WIDTH + (float)SCREEN_HEIGHT / (float)WORLD_SIZE;
+  TILE_SIZE =
+      (float)SCREEN_WIDTH + (float)SCREEN_HEIGHT / (float)WORLD_SIZE * 2;
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "MUZE Game");
   SetTargetFPS(60);
