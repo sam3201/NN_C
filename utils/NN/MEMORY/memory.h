df196bc6e8fe10d1b800c3a929b1ab8214cd73f6
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 14:28:25 -0500

    Auto-save

--- utils/NN/MEMORY/memory.h
+++ utils/NN/MEMORY/memory.h
@@ -14,12 +14,14 @@ typedef struct {
   MemoryEntry *buffer;
   int size;
   int capacity;
-  int index;
+  int input_size; // Store the dimension of vision_inputs here
+  int index;      // Useful if you decide to go back to a circular buffer later
 } Memory;
 
+// Memory interface
 void init_memory(Memory *memory, int initial_capacity, int input_size);
 void store_memory(Memory *memory, long double *vision_inputs, int action,
-                  float reward, float value_estimate, int input_size);
+                  float reward, float value_estimate);
 void free_memory(Memory *memory);
 
 #endif
