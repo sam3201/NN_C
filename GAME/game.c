eefd4a657a1b47e881796468d3aa7d24abeda82f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 18:25:02 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -125,15 +125,31 @@ Color biome_color(int type) {
 }
 
 Color resource_color(ResourceType type) {
-  switch (type) {
-  case RES_TREE:
-    return DARKGREEN;
-  case RES_ROCK:
-    return GRAY;
+  float tree_radius = TILE_SIZE * 0.45f;
+  float rock_radius = TILE_SIZE * 0.35f;
+  float food_radius = TILE_SIZE * 0.25f;
+
+  switch (r->type) {
+
+  case RES_TREE: {
+    // trunk
+    DrawRectangle(s.x - TILE_SIZE * 0.08f, s.y + TILE_SIZE * 0.1f,
+                  TILE_SIZE * 0.16f, TILE_SIZE * 0.45f, BROWN);
+
+    // canopy
+    DrawCircleV((Vector2){s.x, s.y - TILE_SIZE * 0.1f}, tree_radius, DARKGREEN);
+    break;
+  }
+
+  case RES_ROCK: {
+    DrawCircleV(s, rock_radius, DARKGRAY);
+    DrawCircleV((Vector2){s.x + 3, s.y - 3}, rock_radius * 0.5f, GRAY);
+    break;
+  }
+
   case RES_FOOD:
-    return ORANGE;
-  default:
-    return WHITE;
+    DrawCircleV(s, food_radius, ORANGE);
+    break;
   }
 }
 
