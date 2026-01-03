c004697d6b9b55be85dc3b90c41baf1d51d0ddd8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 14:24:33 -0500

    Auto-save

--- utils/NN/MEMORY/memory.c
+++ utils/NN/MEMORY/memory.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void init_memory(Memory *memory, int initial_capacity, int input_size);
-Memory *memory = malloc(sizeof(Memory));
-MemoryEntry *buffer = malloc(sizeof(MemoryEntry) * initial_capacity);
-memory->size = 0;
-memory->index = 0;
-for (int i = 0; i < MEMORY_CAPACITY; i++) {
-  memory->buffer[i].vision_inputs = malloc(input_size * sizeof(long double));
-}
+void init_memory(Memory *memory, int input_size) {
+  memory->size = 0;
+  memory->index = 0;
+  for (int i = 0; i < MEMORY_CAPACITY; i++) {
+    memory->buffer[i].vision_inputs = malloc(input_size * sizeof(long double));
+  }
 }
 
 void store_memory(Memory *memory, long double *vision_inputs, int action,
