f49d82aba8aa5d4d8f1d82f7b2157ee00816630d
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 21:56:00 -0500

    Auto-save

--- GAME/build_game.sh
+++ GAME/build_game.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ---------------- Raylib check ----------------
 if [ ! -d "../utils/Raylib" ]; then
   echo "Raylib not found. Installing..."
   git clone https://github.com/raysan5/raylib.git ../utils/Raylib/
@@ -8,22 +9,34 @@ else
 fi
 
 if [ ! -f "../utils/Raylib/src/libraylib.a" ]; then
-    echo "Building raylib..."
-    cd ../utils/Raylib/src
-    make PLATFORM=PLATFORM_DESKTOP
-    cd ../../../SIMULATION
+  echo "Building Raylib..."
+  cd ../utils/Raylib/src || exit
+  make PLATFORM=PLATFORM_DESKTOP
+  cd ../../GAME || exit
 fi
 
-gcc -w game.c ../utils/NN/NN.c ../utils/NN/MUZE/all.h \
-    -I../utils/Raylib/src \
-    -L../utils/Raylib/src \
-    -lraylib \
-    -framework OpenGL \
-    -framework Cocoa \
-    -framework IOKit \
-    -framework CoreVideo \
-    -o game 
+# ---------------- Compile Game ----------------
+echo "Compiling game..."
 
+# Collect all MuZero source files
+MUZE_SRC="../utils/NN/MUZE/ewc.c \
+../utils/NN/MUZE/growth.c \
+../utils/NN/MUZE/mcts.c \
+../utils/NN/MUZE/muzero_model.c \
+../utils/NN/MUZE/replay_buffer.c \
+../utils/NN/MUZE/selfplay.c \
+../utils/NN/MUZE/toy_env.c \
+../utils/NN/MUZE/trainer.c \
+../utils/NN/MUZE/util.c"
+
+gcc -w game.c $MUZE_SRC \
+    -I../utils/Raylib/src \
+    -I../utils/NN/MUZE \
+    -L../utils/Raylib/src -lraylib \
+    -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
+    -o game
+
+# ---------------- Run ----------------
 if [ $? -eq 0 ]; then
     echo "Compilation successful! Running the game..."
     ./game
@@ -32,6 +45,7 @@ else
     exit 1
 fi
 
-echo "Ran Successfully"
-rm game 
+# Optional: cleanup
+# echo "Cleaning up..."
+# rm game
 
