13b6a5409301f434317152a9d7fe32a76692f78f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 13:15:33 -0500

    Auto-save

--- GAME/environment.c
+++ GAME/environment.c
@@ -8,6 +8,8 @@
 
 static Chunk world[WORLD_SIZE][WORLD_SIZE];
 static bool world_initialized = false;
+Resource resources[MAX_RESOURCES];
+int resource_count = 0;
 
 // ------------------------ helpers --------------------------
 static inline int wrap(int v) { return (v + WORLD_SIZE) % WORLD_SIZE; }
