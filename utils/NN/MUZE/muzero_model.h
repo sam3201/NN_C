9385dea6a80ee9393a397bfcec129951203c634f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 17:04:02 -0500

    Auto-save

--- utils/NN/MUZE/muzero_model.h
+++ utils/NN/MUZE/muzero_model.h
@@ -1,54 +1,29 @@
 #ifndef MUZERO_MODEL_H
 #define MUZERO_MODEL_H
 
-#include <stddef.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* ------------------------
-   MuZero configuration
-   ------------------------ */
 typedef struct {
-  int obs_dim;      // dimension of input observation
-  int latent_dim;   // learned hidden state size
-  int action_count; // number of discrete actions
+  int obs_dim;
+  int latent_dim;
+  int action_count;
 } MuConfig;
 
-/* ------------------------
-   MuZero model structure
-   ------------------------ */
 typedef struct {
   MuConfig cfg;
-
-  /* Weights for the three MuZero networks:
-     - representation f(obs) -> latent
-     - dynamics g(latent, action) -> latent+reward
-     - prediction h(latent) -> policy, value
-
-     NOTE: These are placeholders. Later you will replace them
-     with NN_C layers or parameter buffers.
-  */
   float *repr_W;
   float *dyn_W;
   float *pred_W;
-
   int repr_W_count;
   int dyn_W_count;
   int pred_W_count;
-
 } MuModel;
 
-/* ------------------------
-   Constructor / Destructor
-   ------------------------ */
 MuModel *mu_model_create(const MuConfig *cfg);
 void mu_model_free(MuModel *m);
 
-/* ------------------------
-   MuZero core functions
-   ------------------------ */
 void mu_model_repr(MuModel *m, const float *obs, float *latent_out);
 void mu_model_dynamics(MuModel *m, const float *latent_in, int action,
                        float *latent_out, float *reward_out);
