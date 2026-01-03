57fb02ab3ff84ef5b8ddb54a8a023b315d7776ba
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 22:29:24 -0500

    Auto-save

--- GAME/a.sh
+++ GAME/a.sh
@@ -1,6 +1,6 @@
 MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" | tr '\n' ' ')
 
-clang -fsanitize=address -g -O1 -ogame.c $MUZE_SRC \
+clang -fsanitize=address -g -O1 game.c $MUZE_SRC \
     -I../utils/Raylib/src \
     -I../utils/NN/MUZE \
     -L../utils/Raylib/src -lraylib \
