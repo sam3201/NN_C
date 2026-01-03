ab6a0bec0609828b3fa048ca93987e1484260010
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 14:36:00 -0500

    Auto-save

--- utils/NN/MUZE/growth.h
+++ utils/NN/MUZE/growth.h
@@ -3,8 +3,6 @@
 
 #include "muzero_model.h"
 
-// Expands the latent dimension of a MuModel to a new size.
-// Returns 0 on success, non-zero on failure.
 int mu_model_grow_latent(MuModel *m, int new_latent_dim);
 
 #endif
