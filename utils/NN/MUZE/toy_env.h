22e9b8d83ed13dc3e76be4539f5954f992d9f45f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 21:51:50 -0500

    Auto-save

--- utils/NN/MUZE/toy_env.h
+++ utils/NN/MUZE/toy_env.h
@@ -1,23 +1,17 @@
 #ifndef TOY_ENV_H
 #define TOY_ENV_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 typedef struct {
   int pos;
   int size;
 } ToyEnvState;
 
 typedef void (*env_reset_fn)(void *state, float *obs);
-typedef void (*env_step_fn)(void *state, int action, float *obs, float *reward,
-                            int *done);
+typedef int (*env_step_fn)(void *state, int action, float *obs, float *reward,
+                           int *done);
 
-// initialize/reset
-void toy_env_reset(void *state, float *obs);
-
-// step
+void toy_env_reset(void *state_ptr, float *obs);
 int toy_env_step(void *state_ptr, int action, float *obs, float *reward,
                  int *done);
+
 #endif
