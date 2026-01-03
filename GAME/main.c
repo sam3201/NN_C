d1642aff18455c38ac4b6cf42020d0881d942e8a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 21:11:59 -0500

    Auto-save

--- GAME/main.c
+++ GAME/main.c
@@ -37,6 +37,24 @@ Tank tank_new(Vector2 pos, Color base, Color arm) {
 
 void tank_free(Tank *tank) { free(tank); }
 
+void tank_draw(Tank *t) {
+  // Base
+  DrawCircleV(t->position, t->radius, t->baseColor);
+
+  // Arms (barrels)
+  Vector2 origin = {0, t->armWidth / 2};
+
+  Rectangle arm = {t->position.x, t->position.y, t->armLength, t->armWidth};
+
+  // Main barrel
+  DrawRectanglePro(arm, origin, t->rotation, t->armColor);
+
+  // Secondary arm (slight offset)
+  Rectangle arm2 = arm;
+  arm2.y += t->armWidth + 4;
+  DrawRectanglePro(arm2, origin, t->rotation, t->armColor);
+}
+
 int main(void) {
   printf("Hello, World!");
 
