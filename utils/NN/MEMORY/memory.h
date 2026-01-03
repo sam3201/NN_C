0bc02b6b1045cc7a3e687e0bea1d5bb9d83602ca
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-02 22:37:22 -0500

    Auto-save

---
+++ utils/NN/MEMORY/memory.h
@@ -0,0 +1,27 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "agent.h"
+
+#define MEMORY_CAPACITY 1000
+
+typedef struct {
+  long double *vision_inputs;
+  int action_taken;
+  float reward;
+  float value_estimate;
+} MemoryEntry;
+
+typedef struct {
+  MemoryEntry buffer[MEMORY_CAPACITY];
+  int size;
+  int index; // Circular buffer
+} Memory;
+
+// Memory interface
+void init_memory(Memory *memory, int input_size);
+void store_memory(Memory *memory, long double *vision_inputs, int action,
+                  float reward, float value_estimate, int input_size);
+MemoryEntry *sample_memory(Memory *memory, int idx);
+
+#endif
