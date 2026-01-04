6df26c0eabc6fbd77c09121787f0f543a2cb7edf
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 21:38:08 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -27,7 +27,7 @@ Color gray = (Color){100, 100, 100, 255};
 #define MAX_RESOURCES 512
 #define MAX_MOBS 512
 #define AGENT_PER_TRIBE 8
-#define MAX_AGENTS
+#define MAX_AGENTS AGENT_PER_TRIBE *TRIBE_COUNT
 #define BASE_RADIUS 8
 #define MAX_BASE_PARTICLES 32
 
