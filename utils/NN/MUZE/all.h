de0a89c6c1ddb5f59dfc5b8bc0fd196ea2b98291
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 18:29:19 -0500

    Auto-save

--- utils/NN/MUZE/all.h
+++ utils/NN/MUZE/all.h
@@ -7,13 +7,13 @@
 #include "growth.h"
 #include "mcts.h"
 #include "muze_cortex.h"
+#include "muze_plan.h"
 #include "muzero_model.h"
 #include "replay_buffer.h"
 #include "runtime.h"
 #include "selfplay.h"
 #include "toy_env.h"
 #include "trainer.h"
-#include "muze_plan.h
 #include "util.h"
 
 #endif // MUZE_ALL_H
