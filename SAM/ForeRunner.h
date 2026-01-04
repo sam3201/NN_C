24539ed211a1cdb37838853652f7578669804cd4
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 18:55:48 -0500

    Auto-save

--- SAM/ForeRunner.h
+++ SAM/ForeRunner.h
@@ -1,10 +1,10 @@
 #ifndef FORERUNNER_H
 #define FORERUNNER_H
 
-#include "../utils/NN/TRANSFORMER.h"
 #include "../utils/NN/NN.h"
-#include <stdlib.h>
+#include "../utils/NN/TRANSFORMER.h"
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define NONE 0
 #define TEXT 1
@@ -15,20 +15,24 @@
 
 // ForeRunner Model structure
 typedef struct {
-    Transformer_t* transformer;  // Transformer model for feature extraction
-    unsigned int num_contexts;   // Number of possible contexts
+  Transformer_t *transformer; // Transformer model for feature extraction
+  unsigned int num_contexts;  // Number of possible contexts
 } ForeRunner_t;
 
 // Initialize the ForeRunner model
-ForeRunner_t* ForeRunner_init(size_t input_dim, size_t num_heads, unsigned int num_contexts);
+ForeRunner_t *ForeRunner_init(size_t input_dim, size_t num_heads,
+                              unsigned int num_contexts);
 
 // Compute context from input data
-unsigned int ForeRunner_forward(ForeRunner_t* model, long double** input_sequence, unsigned int seq_length);
-    
+unsigned int ForeRunner_forward(ForeRunner_t *model,
+                                long double **input_sequence,
+                                unsigned int seq_length);
+
 // Backpropagation function
-void ForeRunner_backprop(ForeRunner_t* model, long double** input_sequence, unsigned int seq_length, long double* grad_loss);
+void ForeRunner_backprop(ForeRunner_t *model, long double **input_sequence,
+                         unsigned int seq_length, long double *grad_loss);
 
 // Destroy the ForeRunner model
-void ForeRunner_destroy(ForeRunner_t* model);
+void ForeRunner_destroy(ForeRunner_t *model);
 
 #endif // FORERUNNER_H
