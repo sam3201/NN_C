f1a1a6a6d9c1e21cafc413150e4318265003e99f
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 00:35:00 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -469,13 +469,10 @@ void init_tribes(void) {
   }
 }
 
-void draw_base(void) {
-  for (int t = 0; t < TRIBE_COUNT; t++) {
-    Tribe *tr = &tribes[t];
-    DrawCircle(tr->base.position.x * TILE_SIZE, tr->base.position.y * TILE_SIZE,
-               tr->base.radius * TILE_SIZE, Fade(tr->color, 0.3f));
-  }
-}
+/* =======================
+  BASE
+======================= */
+void draw_base(Tribe *tr) {}
 
 /* =======================
    AGENTS
