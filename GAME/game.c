66099cf5bff5102dff530d29140a0f87a469d7a7
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 21:37:22 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -24,7 +24,7 @@ Color gray = (Color){100, 100, 100, 255};
 
 #define MAX_RESOURCES 512
 #define MAX_MOBS 512
-#define MAX_AGENTS 32
+#define MAX_AGENTS
 #define BASE_RADIUS 8
 #define MAX_BASE_PARTICLES 32
 
