215a5a0a3de139db8bfb6e9f61a54218c07087ab
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 19:55:06 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -16,6 +16,8 @@ int SCREEN_HEIGHT;
 float TILE_SIZE;
 
 Color pink = (Color){255, 192, 203, 255};
+Color yellow = (Color){255, 255, 0, 255};
+Color gray = (Color){100, 100, 100, 255};
 
 #define WORLD_SIZE 128
 #define CHUNK_SIZE 32
