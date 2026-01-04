f708a057fc1196eeeb953b5d0b614a0576393066
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 23:48:15 -0500

    Auto-save

--- SIMULATION/sim.c
+++ SIMULATION/sim.c
@@ -631,8 +631,7 @@ void update_game(GameState *game) {
         ((float)rand() / RAND_MAX) < FOOD_SPAWN_CHANCE)
       spawn_food(&game->food[i]);
 
-  for (int i = 0; i < POPULATION_SIZE - MAX_GROUNDSKEEPERS; i++)
-    update_agent(game, i);
+  update_agents(game, i);
 
   for (int i = 0; i < MAX_GROUNDSKEEPERS; i++)
     update_groundkeeper(game, i);
