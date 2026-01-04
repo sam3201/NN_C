0e3f14aa7ec6b3b36052955626c343271e49facb
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 19:51:51 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -32,6 +32,7 @@ float TILE_SIZE;
 
 typedef enum { TOOL_HAND = 0, TOOL_AXE, TOOL_PICKAXE, TOOL_NONE } ToolType;
 typedef enum { RES_TREE = 0, RES_ROCK, RES_FOOD, RES_NONE } ResourceType;
+
 typedef enum {
   MOB_PIG = 0,
   MOB_SHEEP,
