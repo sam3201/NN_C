ab8a4675e729a6eb0ca8385f6ad5efe8df45e9d1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 23:49:47 -0500

    Auto-save

--- SIMULATION/sim.c
+++ SIMULATION/sim.c
@@ -460,7 +460,8 @@ void update_latent_after_step(Agent *agent, long double *obs, int action,
   mu_model_dynamics(agent->brain, agent->latent, action, next_latent,
                     &predicted_reward);
 
-  memcpy(agent->latent, next_latent, sizeof(float) * LATENT_MAX);
+  int L = agent->brain->cfg.latent_dim;
+  memcpy(agent->latent, next_latent, sizeof(float) * L);
 }
 
 void step_agent(GameState *state, Agent *agent, int action) {
