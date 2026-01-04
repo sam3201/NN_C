4bb095486f1c4642c6725a093ce23acd7501b1b8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 18:37:03 -0500

    Auto-save

--- GAME/a.sh
+++ GAME/a.sh
@@ -1,8 +1,10 @@
 MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" | tr '\n' ' ')
+SAM_SRC=$(find ../utils/NN/SAM -name "*.c" | tr '\n' ' ')
 
-gcc -g game.c $MUZE_SRC \
+gcc -w game.c $MUZE_SRC $SAM_SRC \
     -I../utils/Raylib/src \
     -I../utils/NN/MUZE \
+    -I../utils/NN/SAM \
     -L../utils/Raylib/src -lraylib \
     -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
     -o game
