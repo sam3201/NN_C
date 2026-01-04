8e334a10302ae3e4568cce9a1e34807a87999ac2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-03 19:39:57 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -396,8 +396,7 @@ void draw_ui(Player player) {
   DrawText("Stamina:", 10, 30, 16, WHITE);
   DrawText("Age:", 10, 50, 16, WHITE);
 
-  long double FPS = GetFPS();
-  DrawText(TextFormat("FPS: %d", (int)FPS), 10, 70, 16, WHITE);
+  DrawText(TextFormat("FPS: %d", (int)GetFPS()), 10, 70, 16, WHITE);
 }
 
 /* =======================
