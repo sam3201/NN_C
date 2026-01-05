84ac9a6a84379eec209fa73ff2a4d3395ab25f8a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 16:06:39 -0500

    Auto-save

--- utils/NN/NEAT.c
+++ utils/NN/NEAT.c
@@ -372,10 +372,8 @@ Genome_t *GENOME_crossover(Genome_t *p1, Genome_t *p2) {
   // Rebuild NN_t from node & connection structure
   size_t layers[2] = {numInputs, numOutputs}; // simple input->output NN
 
-  ActivationFunctionType actFunc1 =
-      (RegularizationType)rand() % REGULARIZATION_TYPE_COUNT;
-  ActivationFunctionType actFunc2 =
-      (RegularizationType)rand() % REGULARIZATION_TYPE_COUNT;
+  ActivationFunctionType actFunc1 = rand() % REGULARIZATION_TYPE_COUNT;
+  ActivationFunctionType actFunc2 = rand() % REGULARIZATION_TYPE_COUNT;
   ActivationFunctionType actFuncs[2] = {
       actFunc1,
       actFunc2,
