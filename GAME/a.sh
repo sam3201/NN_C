164b23299973ffbc30a4e4259d40c1251f3cf9fe
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 16:44:26 -0500

    Auto-save

--- GAME/a.sh
+++ GAME/a.sh
@@ -3,8 +3,6 @@
 
 gcc -g game.c \
     -I../utils/Raylib/src \
-    -I../utils/NN/MUZE \
-    -I../utils/NN/SAM \
     -L../utils/Raylib/src -lraylib \
     -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
     -o game
