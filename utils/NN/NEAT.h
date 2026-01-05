975f7ae0f9a5b7572793eb4b06288795b2f91cae
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 21:20:43 -0500

    Auto-save

--- utils/NN/NEAT.h
+++ utils/NN/NEAT.h
@@ -64,7 +64,7 @@ void GENOME_add_connection(Genome_t *genome, size_t fromNode, size_t toNode,
 
 void GENOME_mutate_weights(Genome_t *genome, long double perturbRate,
                            long double perturbAmount);
-Genome_t *GENOME_crossover(Genome_t *parent1, Genome_t *parent2);
+Genome_t *GENOME_crossover(Genome_t *p1, Genome_t *p2);
 
 // ------------------- Forward Propagation -------------------
 void GENOME_forwward(Genome_t *genome, long double *input, long double *output);
