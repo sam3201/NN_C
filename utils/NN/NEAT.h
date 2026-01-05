ed1f5e82086e7cae6818c6f14b465c733d1d479e
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 15:39:54 -0500

    Auto-save

--- utils/NN/NEAT.h
+++ utils/NN/NEAT.h
@@ -68,8 +68,7 @@ Genome_t *GENOME_init(size_t *layers, ActivationFunctionType *actFuncs,
                       ActivationDerivativeType *actDerivs,
                       LossFunctionType lossFunc, LossDerivativeType lossDeriv,
                       RegularizationType reg, OptimizerType opt,
-                      long double learningRate, size_t numInputs,
-                      size_t numOutputs);
+                      long double learningRate, size_t numInputs, );
 void GENOME_destroy(Genome_t *genome);
 
 // ------------------- NEAT Operators -------------------
