7fe2413ced5e3312412b90d7e8de25e9f8579ff1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 15:51:58 -0500

    Auto-save

--- utils/NN/NEAT.c
+++ utils/NN/NEAT.c
@@ -375,8 +375,8 @@ Genome_t *GENOME_crossover(Genome_t *p1, Genome_t *p2) {
       (RegularizationType)rand() % REGULARIZATION_TYPE_COUNT,
       (OptimizerType)rand() % OPTIMIZER_TYPE_COUNT};
   child->nn = NN_init(layers, actFuncs, NULL,
-                      (LossFunctionType)rand() % LOSS_TYPE_COUNT, NULL, NULL,
-                      SGD, 0.01L);
+                      (LossFunctionType)rand() % LOSS_TYPE_COUNT,
+                      (OptimizerType)rand() % OPTIMIZER_TYPE_COUNT, 0.01L);
 
   return child;
 }
