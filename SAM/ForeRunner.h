8b33ff1dcc7ff8367671bcff75f527f2042f35f7
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 18:53:15 -0500

    Auto-save

--- SAM/ForeRunner.h
+++ SAM/ForeRunner.h
@@ -26,34 +26,7 @@ ForeRunner_t *ForeRunner_init(size_t input_dim, size_t num_heads,
 // Compute context from input data
 unsigned int ForeRunner_forward(ForeRunner_t *model,
                                 long double **input_sequence,
-                                unsigned int seq_length) {
-  if (!model || !input_sequence || seq_length == 0)
-    return NONE;
-
-  long double *logits =
-      TRANSFORMER_forward(model->transformer, input_sequence, seq_length);
-  if (!logits)
-    return NONE;
-
-  unsigned int C = model->num_contexts;
-  if (C == 0) {
-    free(logits);
-    return NONE;
-  }
-
-  /* argmax over first C outputs */
-  unsigned int best = 0;
-  long double bestv = logits[0];
-  for (unsigned int i = 1; i < C; i++) {
-    if (logits[i] > bestv) {
-      bestv = logits[i];
-      best = i;
-    }
-  }
-
-  free(logits);
-  return best;
-}
+                                unsigned int seq_length);
 
 // Backpropagation function
 void ForeRunner_backprop(ForeRunner_t *model, long double **input_sequence,
