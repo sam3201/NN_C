60a2a45b1aa8e20e8b5b06a117d2ded655446e59
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 21:19:13 -0500

    Auto-save

--- utils/NN/NEAT.h
+++ utils/NN/NEAT.h
@@ -73,7 +73,7 @@ long double activate(long double x, ActivationFunctionType type);
 void GENOME_save(Genome_t *genome, const char *filename);
 Genome_t *GENOME_load(const char *filename);
 
-// Return innovation number, create if new
+// ------------------- Innovation Number -------------------
 size_t get_innovation_number(size_t from, size_t to);
 
 // ------------------- Topological Sort -------------------
