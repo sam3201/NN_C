e1ed38621b9661117be60d11ca2b6049ec92c667
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-04 23:36:38 -0500

    Auto-save

--- utils/NN/transformer.h
+++ utils/NN/transformer.h
@@ -83,9 +83,9 @@ long double *transformer_mha_forward(MultiHeadAttention *mha,
 long double *transformer_norm_forward(LayerNorm *ln, long double *input);
 long double *transformer_layer_forward(TransformerLayer *layer,
                                        long double *input);
-long double *TRANSFORMER_forward(Transformer_t *transformer,
-                                 long double **input_sequence,
-                                 size_t seq_length);
+long double **TRANSFORMER_forward(Transformer_t *transformer,
+                                  long double **input_sequence,
+                                  size_t seq_length);
 
 // Backpropagation
 void transformer_mha_backprop(MultiHeadAttention *mha,
