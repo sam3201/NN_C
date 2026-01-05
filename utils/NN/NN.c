d106796c2f6dcd85ca85b4b909a57f0780bfe5e2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 13:15:14 -0500

    Auto-save

--- utils/NN/NN.c
+++ utils/NN/NN.c
@@ -559,9 +559,12 @@ void NN_backprop_custom_delta(NN_t *nn, long double inputs[],
     size_t out_size = nn->layers[l + 1];
 
     for (size_t j = 0; j < out_size; j++) {
-      for (size_t i = 0; i < in_size; i++)
-        nn->biases_grad[l][j] = deltas[l][j];
-      nn->weights_grad[l][i * out_size + j] = activations[l][i] * deltas[l][j];
+      nn->biases_grad[l][j] = deltas[l][j];
+
+      for (size_t i = 0; i < in_size; i++) {
+        nn->weights_grad[l][i * out_size + j] =
+            activations[l][i] * deltas[l][j];
+      }
     }
   }
 
