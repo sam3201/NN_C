f44eb1ae64fc53d90e764d7e00728f7900c417b8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 15:38:11 -0500

    Auto-save

--- utils/NN/NEAT.c
+++ utils/NN/NEAT.c
@@ -22,8 +22,7 @@ Genome_t *GENOME_init(size_t *layers, ActivationFunctionType *actFuncs,
                       ActivationDerivativeType *actDerivs,
                       LossFunctionType lossFunc, LossDerivativeType lossDeriv,
                       RegularizationType reg, OptimizerType opt,
-                      long double learningRate, size_t numInputs,
-                      size_t numOutputs) {
+                      long double learningRate, size_t numInputs) {
   Genome_t *genome = (Genome_t *)malloc(sizeof(Genome_t));
   genome->numNodes = numInputs + numOutputs + 2; // initial + bias
   genome->nodes = (Node **)malloc(genome->numNodes * sizeof(Node *));
