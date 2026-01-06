40d390dc07a7eb44634eabfeeb75ef779317863a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 19:21:03 -0500

    Auto-save

--- GAME/build_game.sh
+++ GAME/build_game.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 RAYLIB_DIR="../utils/Raylib"
 RAYLIB_LIB="$RAYLIB_DIR/src/libraylib.a"
@@ -18,45 +18,33 @@ fi
 
 echo "Collecting sources..."
 
-# Build arrays (no whitespace bugs)
-mapfile -t MUZE_FILES < <(find ../utils/NN/MUZE -name "*.c" -print)
-mapfile -t SAM_FILES  < <(find ../SAM -name "*.c" -print)
+MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" -print | tr '\n' ' ')
+SAM_SRC=$(find ../SAM -name "*.c" -print | tr '\n' ' ')
+NN_SRC="../utils/NN/NN.c ../utils/NN/TRANSFORMER.c ../utils/NN/NEAT.c"
 
-NN_FILES=(../utils/NN/NN.c ../utils/NN/TRANSFORMER.c ../utils/NN/NEAT.c)
+# Trim leading/trailing spaces (prevents the ' ' phantom arg)
+MUZE_SRC=$(printf "%s" "$MUZE_SRC" | sed 's/^ *//; s/ *$//')
+SAM_SRC=$(printf "%s" "$SAM_SRC" | sed 's/^ *//; s/ *$//')
 
-# Sanity checks so you get a useful error
-if [ ${#MUZE_FILES[@]} -eq 0 ]; then
+if [ -z "$MUZE_SRC" ]; then
   echo "ERROR: No MUZE .c files found under ../utils/NN/MUZE"
   exit 1
 fi
 
-if [ ${#SAM_FILES[@]} -eq 0 ]; then
+if [ -z "$SAM_SRC" ]; then
   echo "ERROR: No SAM .c files found under ../SAM"
   exit 1
 fi
 
 echo "Compiling game..."
 
-gcc -w \
-  game.c \
-  "${NN_FILES[@]}" \
-  "${MUZE_FILES[@]}" \
-  "${SAM_FILES[@]}" \
-  -I../utils/NN \
-  -I../utils/NN/MUZE \
-  -I../SAM \
-  -I../utils/Raylib/src \
-  -L../utils/Raylib/src \
-  -lraylib \
-  -framework OpenGL \
-  -framework Cocoa \
-  -framework IOKit \
-  -framework CoreVideo \
+gcc -w game.c $NN_SRC $MUZE_SRC $SAM_SRC \
+  -I../utils/NN -I../utils/NN/MUZE -I../SAM -I../utils/Raylib/src \
+  -L../utils/Raylib/src -lraylib \
+  -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
   -arch arm64 \
   -o game
 
 echo "Compilation successful! Running the game..."
 ./game
-
-rm -f game
 
