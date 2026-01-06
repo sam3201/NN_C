22d9ef60bb571587be261be729ed3f1bf875404c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 21:26:59 -0500

    Auto-save

--- utils/NN/MUZE/muze_plan.c
+++ utils/NN/MUZE/muze_plan.c
@@ -31,10 +31,17 @@ int muze_plan(MuCortex *cortex, float *obs, size_t obs_dim,
   }
 
   /* cleanup */
-  for (size_t i = 0; i < seq_len; i++)
-    free(latent_seq[i]);
-  free(latent_seq);
+  /* cleanup */
+  if (cortex->free_latent_seq) {
+    cortex->free_latent_seq(cortex->brain, latent_seq, seq_len);
+  } else {
+    for (size_t i = 0; i < seq_len; i++)
+      free(latent_seq[i]);
+    free(latent_seq);
+  }
+
   free(action_probs);
+  return best;
 
   return best;
 }
