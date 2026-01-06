5cc9d74e5c21fccc22e97eabaa2bb92abdd00369
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 14:05:51 -0500

    Auto-save

--- utils/TESTS/muzero_replay_test.c
+++ utils/TESTS/muzero_replay_test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 static int argmax_policy_from_model(MuModel *m, const float *obs) {
   int A = m->cfg.action_count;
