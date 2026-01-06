1b964a8733f0e3631770376dd2f3c77a1e869ef1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 19:18:15 -0500

    Auto-save

--- GAME/build_game.sh
+++ GAME/build_game.sh
@@ -1,38 +1,63 @@
 #!/bin/bash
+set -euo pipefail
 
-if [ ! -d "../utils/Raylib" ]; then
+RAYLIB_DIR="../utils/Raylib"
+RAYLIB_LIB="$RAYLIB_DIR/src/libraylib.a"
+
+if [ ! -d "$RAYLIB_DIR" ]; then
   echo "Raylib not found. Installing..."
-  git clone https://github.com/raysan5/raylib.git ../utils/Raylib/
+  git clone https://github.com/raysan5/raylib.git "$RAYLIB_DIR"
 else
   echo "Raylib found. Skipping installation."
 fi
 
-if [ ! -f "../utils/Raylib/src/libraylib.a" ]; then
+if [ ! -f "$RAYLIB_LIB" ]; then
   echo "Building Raylib..."
-  cd ../utils/Raylib/src || exit
-  make PLATFORM=PLATFORM_DESKTOP
-  cd ../../GAME || exit
+  (cd "$RAYLIB_DIR/src" && make PLATFORM=PLATFORM_DESKTOP)
+fi
+
+echo "Collecting sources..."
+
+# Build arrays (no whitespace bugs)
+mapfile -t MUZE_FILES < <(find ../utils/NN/MUZE -name "*.c" -print)
+mapfile -t SAM_FILES  < <(find ../SAM -name "*.c" -print)
+
+NN_FILES=(../utils/NN/NN.c ../utils/NN/TRANSFORMER.c ../utils/NN/NEAT.c)
+
+# Sanity checks so you get a useful error
+if [ ${#MUZE_FILES[@]} -eq 0 ]; then
+  echo "ERROR: No MUZE .c files found under ../utils/NN/MUZE"
+  exit 1
+fi
+
+if [ ${#SAM_FILES[@]} -eq 0 ]; then
+  echo "ERROR: No SAM .c files found under ../SAM"
+  exit 1
 fi
 
 echo "Compiling game..."
 
-MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" | tr '\n' ' ')
-NN_SRC="../utils/NN/NN.c ../utils/NN/TRANSFORMER.c ../utils/NN/NEAT.c"
-SAM_SRC=$(find ../SAM -name "*.c" | tr '\n' ' ')
+gcc -w \
+  game.c \
+  "${NN_FILES[@]}" \
+  "${MUZE_FILES[@]}" \
+  "${SAM_FILES[@]}" \
+  -I../utils/NN \
+  -I../utils/NN/MUZE \
+  -I../SAM \
+  -I../utils/Raylib/src \
+  -L../utils/Raylib/src \
+  -lraylib \
+  -framework OpenGL \
+  -framework Cocoa \
+  -framework IOKit \
+  -framework CoreVideo \
+  -arch arm64 \
+  -o game
 
-gcc -w game.c $NN_SRC $MUZE_SRC $SAM_SRC \
-    -I../utils/NN/ -I../utils/NN/MUZE  -I../utils/NN/SAM -I../utils/Raylib/src \ 
-    -L../utils/Raylib/src -lraylib -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
-    -arch arm64 \
-    -o game
+echo "Compilation successful! Running the game..."
+./game
 
-if [ $? -eq 0 ]; then
-    echo "Compilation successful! Running the game..."
-    ./game
-else
-    echo "Compilation failed!"
-    exit 1
-fi
-
-rm game
+# Only remove if you really want it removed every run:
+# rm -f game
 
