1b3742994c484b0a4c713256705ac77f6ba2a366
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 13:14:24 -0500

    Auto-save

--- utils/TESTS/build_test.sh
+++ utils/TESTS/build_test.sh
@@ -1,4 +1,4 @@
-clang -g -O0 -fsanitize=address,undefined -fno-omit-frame-pointer \
+gcc -g -O0 -fsanitize=address,undefined -fno-omit-frame-pointer \
   -I../../SAM -I../NN -I../NN/MUZE -I../NN/MEMORY -I../NN/TOKENIZER -I../NN/TRANSFORMER \
   sam_muze_toy_test.c \
   ../NN/NEAT.c ../NN/NN.c ../NN/TOKENIZER.c ../NN/TRANSFORMER.c ../../SAM/SAM.c ../../SAM/SAM_MUZE_ADAPTER.c \
