e70e3a927547a7b438f171f481e09f0a33305bf4
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 14:05:42 -0500

    Auto-save

--- utils/TESTS/muzero_replay_test.c
+++ utils/TESTS/muzero_replay_test.c
@@ -126,6 +126,8 @@ int main(void) {
   printf("Training from replay...\n");
   time_t start = time(NULL);
   trainer_train_from_replay(model, rb, &tc);
+  time_t end = time(NULL);
+  printf("Training took %ld seconds\n", end - start);
 
   // ---- eval AFTER ----
   float after = eval_success_rate(model, 100, 128);
