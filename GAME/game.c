388f05163033b122a0772afd848604a5a276629e
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 21:11:42 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -434,7 +434,7 @@ void update_agent(Agent *a) {
   encode_observation(a, c, &obs);
 
   MuCortex *cortex = tribes[a->agent_id / AGENT_PER_TRIBE].cortex;
-  int action = muze_plan(cortexobs.data);
+  int action = muze_plan(cortex, obs.data, obs.size, ACTION_COUNT);
 
   obs_free(&obs);
 
