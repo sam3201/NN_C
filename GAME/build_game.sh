2103f6ffc3e6f994bd26e2138c65d7a41d4883e5
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 19:29:31 -0500

    Auto-save

--- GAME/build_game.sh
+++ GAME/build_game.sh
@@ -15,6 +15,8 @@ if [ ! -f "../utils/Raylib/src/libraylib.a" ]; then
 fi
 
 MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" | tr '\n' ' ')
+NN_SRC=$(find ../utils/NN -name "*.c" | tr '\n' ' ')
+SAM_SRC=$(find ../SAM -name "*.c" | tr '\n' ' ')
 
 gcc -w game.c $NN_SRC $MUZE_SRC $SAM_SRC \
   -I../utils/NN -I../utils/NN/MUZE -I../SAM -I../utils/Raylib/src \
