f5bb7761b932e50ba02fa353f128908cfeb6d3c2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 00:14:45 -0500

    Auto-save

--- utils/NN/TOKENIZER.h
+++ utils/NN/TOKENIZER.h
@@ -7,16 +7,16 @@
 #define MAX_TOKEN_LENGTH 50
 
 typedef struct {
-    char** vocabulary;
-    size_t vocab_size;
-    size_t max_sequence_length;
+  char **vocabulary;
+  size_t vocab_size;
+  size_t max_sequence_length;
 } Tokenizer;
 
 // Function declarations
-Tokenizer* create_tokenizer(const char* corpus_path);
-void tokenizer_fit(Tokenizer* tokenizer, const char* text);
-long double* tokenizer_encode(Tokenizer* tokenizer, const char* text);
-char* tokenizer_decode(Tokenizer* tokenizer, const long double* encoded);
-void free_tokenizer(Tokenizer* tokenizer);
+Tokenizer *create_tokenizer(const char *corpus_path);
+void tokenizer_fit(Tokenizer *tokenizer, const char *text);
+long double *tokenizer_encode(Tokenizer *tokenizer, const char *text);
+char *tokenizer_decode(Tokenizer *tokenizer, const long double *encoded);
+void free_tokenizer(Tokenizer *tokenizer);
 
 #endif // TOKENIZER_H
