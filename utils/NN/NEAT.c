47f305345bcea32066fc9879c888ac8106510c23
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 12:32:55 -0500

    Auto-save

--- utils/NN/NEAT.c
+++ utils/NN/NEAT.c
@@ -653,6 +653,13 @@ void POPULATION_evolve(Population *pop) {
     Genome_t *g1 = pop->genomes[p1];
     Genome_t *g2 = pop->genomes[p2];
     nextGen[i] = GENOME_crossover(g1, g2);
+    if (!g1 || !g2) {
+      // fallback: clone best genome instead of crashing
+      // nextGen[i] = GENOME_clone(pop->genomes[0]);
+      // or if you don't have clone: nextGen[i] =
+      // GENOME_crossover(pop->genomes[0], pop->genomes[0]);
+      continue;
+    }
 
     GENOME_mutate(nextGen[i], 0.8L, 0.2L, 0.3L, 0.5L, 0.1L, 0.05L, 0.05L);
   }
