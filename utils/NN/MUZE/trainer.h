9a77546eeb3a18e95c56ee170145fe60f2bf9470
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 13:46:15 -0500

    Auto-save

---
+++ utils/NN/MUZE/trainer.h
@@ -0,0 +1,9 @@
+typedef struct TrainerConfig {
+  int batch_size;
+  int train_steps;     // how many SGD steps per "train call"
+  int min_replay_size; // warmup
+  float lr;
+} TrainerConfig;
+
+void trainer_train_from_replay(MuZeroModel *model, ReplayBuffer *rb,
+                               const TrainerConfig *cfg);
