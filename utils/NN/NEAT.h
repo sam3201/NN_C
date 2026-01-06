8939b6180e1c31a198f97c47d35fc059eb9cc435
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 01:14:24 -0500

    Auto-save

--- utils/NN/NEAT.h
+++ utils/NN/NEAT.h
@@ -60,8 +60,8 @@ typedef struct {
   size_t output_dims;
 } NEAT_t;
 
-static InnovationRecord *innovationHistory;
-static size_t innovationCount;
+extern InnovationRecord *innovationHistory;
+extern size_t innovationCount;
 
 // ------------------- Constructors / Destructors -------------------
 Genome_t *GENOME_init(size_t *layers, ActivationFunctionType *actFuncs,
