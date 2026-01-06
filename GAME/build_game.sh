b123cd64b9f4bebdc988053549641f15fb0deefc
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-05 19:29:22 -0500

    Auto-save

--- GAME/build_game.sh
+++ GAME/build_game.sh
@@ -16,22 +16,12 @@ fi
 
 MUZE_SRC=$(find ../utils/NN/MUZE -name "*.c" | tr '\n' ' ')
 
-gcc -w game.c $MUZE_SRC \
-    ../utils/NN/NN.c \
-    ../utils/NN/NEAT.c \
-    ../utils/NN/TRANSFORMER.c \
-    ../SAM/SAM.c \
-    ../utils/NN/MEMORY/MEMORY.c \
-    -I../utils/NN \
-    -I../utils/NN/MEMORY \
-    -I../utils/Raylib/src \
-    -L../utils/Raylib/src \
-    -lraylib \
-    -framework OpenGL \
-    -framework Cocoa \
-    -framework IOKit \
-    -framework CoreVideo \
-    -o sim
+gcc -w game.c $NN_SRC $MUZE_SRC $SAM_SRC \
+  -I../utils/NN -I../utils/NN/MUZE -I../SAM -I../utils/Raylib/src \
+  -L../utils/Raylib/src -lraylib \
+  -framework OpenGL -framework Cocoa -framework IOKit -framework CoreVideo \
+  -arch arm64 \
+  -o game
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful! Running the game..."
