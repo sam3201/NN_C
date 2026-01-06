4c989c9a3a342ad86812bc439999933dcb66f560
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 14:57:57 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -536,7 +536,7 @@ int main(void) {
         Agent *ai = &agents[a];
         ObsBuffer obs;
         obs_init(&obs);
-        encode_observation(ai, ai->chunk, &ai->obs);
+        encode_observation(ai, ai->cortex->chunk, &obs);
         ai->cortex->learn(ai->cortex->brain, obs.data, ai->obs.size, ai->action,
                           ai->reward_accumulator, 0);
         ai->reward_accumulator = 0.0f;
