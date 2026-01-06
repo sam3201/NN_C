e31cc2a28c6d13f9ed841ec3a39aba19a4ddafb2
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 13:51:44 -0500

    Auto-save

--- utils/NN/MUZE/trainer.h
+++ utils/NN/MUZE/trainer.h
@@ -1,12 +1,26 @@
+// MUZE/trainer.h
+#ifndef MUZE_TRAINER_H
+#define MUZE_TRAINER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "muzero_model.h"
 #include "replay_buffer.h"
 
 typedef struct TrainerConfig {
   int batch_size;
-  int train_steps;     // how many SGD steps per "train call"
-  int min_replay_size; // warmup
+  int train_steps;     // how many SGD steps per call
+  int min_replay_size; // warmup threshold
   float lr;
 } TrainerConfig;
 
 void trainer_train_from_replay(MuModel *model, ReplayBuffer *rb,
                                const TrainerConfig *cfg);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MUZE_TRAINER_H
