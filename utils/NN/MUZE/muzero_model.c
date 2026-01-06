7c11f20bf96caae5ecb1a27c92055a51dd211404
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 16:13:02 -0500

    Auto-save

--- utils/NN/MUZE/muzero_model.c
+++ utils/NN/MUZE/muzero_model.c
@@ -133,11 +133,6 @@ void mu_model_predict(MuModel *m, const float *latent_in,
     m->predict(m, latent_in, policy_logits_out, value_out);
     return;
   }
-
-  if (m->predict)
-    m->predict(m, latent_in, policy_logits_out, value_out);
-  else
-    default_predict(m, latent_in, policy_logits_out, value_out);
 }
 
 void mu_model_step(MuModel *m, const float *obs, int action, float reward) {
