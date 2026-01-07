e1d8df22866abb7e2187fc0ae1caf7d568cd5216
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-07 14:16:28 -0500

    Auto-save

---
+++ SAM/SAM_MUZE_BRIDGE.h
@@ -0,0 +1,21 @@
+#ifndef SAM_MUZE_BRIDGE_H
+#define SAM_MUZE_BRIDGE_H
+
+#include "../utils/NN/MUZE/muze_cortex.h"
+#include "SAM.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Creates a MUZE cortex wrapper around SAM. Caller owns returned MuCortex*. */
+MuCortex *SAM_as_MUZE(SAM_t *sam);
+
+/* Frees cortex created by SAM_as_MUZE(). */
+void SAM_MUZE_destroy(MuCortex *cortex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SAM_MUZE_BRIDGE_H
