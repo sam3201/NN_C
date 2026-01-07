7d7bbf68e68edcfc68fe5ab41de94e39b9adcf58
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 22:01:53 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -1679,7 +1679,6 @@ void update_player(void) {
   if (isKeyDown(KEY_TAB)) {
     crafting_open = !crafting_open;
     if (crafting_open) {
-      // press 1..9 to craft
       for (int i = 0; i < recipe_count && i < 9; i++) {
         if (IsKeyPressed((KeyboardKey)(KEY_ONE + i))) {
           craft(&recipes[i]);
