f2698ed69d729f21805f460b1d9bfcd8837be4ae
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-06 22:18:19 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -2069,6 +2069,15 @@ int main(void) {
     EndDrawing();
   }
 
+  pthread_mutex_lock(&job_mtx);
+  job_quit = 1;
+  pthread_cond_broadcast(&job_cv);
+  pthread_mutex_unlock(&job_mtx);
+
+  for (int i = 0; i < WORKER_COUNT; i++) {
+    pthread_join(workers[i], NULL);
+  }
+
   CloseWindow();
   return 0;
 }
