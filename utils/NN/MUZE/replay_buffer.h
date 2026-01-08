6d33a72a4d4538c554ff8019f7dbb8418e960d47
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 13:39:00 -0500

    Auto-save

--- utils/NN/MUZE/replay_buffer.h
+++ utils/NN/MUZE/replay_buffer.h
@@ -12,6 +12,9 @@ typedef struct ReplayBuffer ReplayBuffer;
 ReplayBuffer *rb_create(size_t capacity, int obs_dim, int action_count);
 void rb_free(ReplayBuffer *rb);
 void rb_push(ReplayBuffer *rb, const float *obs, const float *pi, float z);
+void rb_push_transition(ReplayBuffer *rb, const float *obs, int action,
+                        float reward, const float *next_obs, int done);
+
 int rb_sample(ReplayBuffer *rb, int batch, float *obs_batch, float *pi_batch,
               float *z_batch);
 size_t rb_size(ReplayBuffer *rb);
