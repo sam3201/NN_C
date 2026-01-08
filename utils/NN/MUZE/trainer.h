5c02938280f069dc1189f26a24c44d28ffccd490
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 13:42:23 -0500

    Auto-save

--- utils/NN/MUZE/trainer.h
+++ utils/NN/MUZE/trainer.h
@@ -15,8 +15,10 @@ typedef struct TrainerConfig {
   float lr;
 } TrainerConfig;
 
-void trainer_train_from_replay(MuModel *model, ReplayBuffer *rb,
-                               const TrainerConfig *cfg);
+vvoid trainer_train_dynamics(MuModel *model, ReplayBuffer *rb,
+                             const TrainerConfig *cfg);
+oid trainer_train_from_replay(MuModel *model, ReplayBuffer *rb,
+                              const TrainerConfig *cfg);
 
 #ifdef __cplusplus
 }
