f4a3b8c708882f4d84265730624c6fd2dc850b18
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 12:16:27 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -4308,7 +4308,7 @@ case ACTION_CRAFT:
       // auto-face while latched too (so it doesn't keep shooting the old
       // direction)
       pthread_rwlock_rdlock(&c->lock);
-      agent_face_nearest_mob_in_chunk(a, c, cx, cy, 14.0f);
+      // agent_face_nearest_mob_in_chunk(a, c, cx, cy, 14.0f);
       pthread_rwlock_unlock(&c->lock);
 
       agent_try_fire_forward(a, tr, &reward, true);
