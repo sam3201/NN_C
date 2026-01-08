c8d48048afe4f21f900be381e78f06e7c98f5cde
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-07 23:04:59 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -1213,7 +1213,7 @@ static int load_models_from_disk(const char *world_name) {
       continue;
 
     // DO THIS (real API):
-    // SAM_load(f, a->sam);
+    SAM_load(f, a->sam);
 
     fclose(f);
   }
