cf450c900eec53dec6d02a341d97814739bd7c7a
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 00:55:01 -0500

    Auto-save

--- HUMANOID/sim.c
+++ HUMANOID/sim.c
@@ -681,7 +681,7 @@ void update_agent(Agent *a) {
 
     // One last learn call to close the episode
     if (a->cortex && a->has_last_transition) {
-      a->cortex->learn(a->cortex->brain, a->last_obs.obs, (size_t)OBS_DIM,
+      a->cortex->end_episode(a->cortex->brain, a->last_obs.obs, (size_t)OBS_DIM,
                        a->last_action, a->pending_reward, 1 /*terminal*/);
     }
     a->pending_reward = 0.0f;
