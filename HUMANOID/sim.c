9fdbd83dd945942fc79f9fdc3f38bd8558b9cfb9
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 01:09:44 -0500

    Auto-save

--- HUMANOID/sim.c
+++ HUMANOID/sim.c
@@ -922,8 +922,6 @@ int main(void) {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    float groundY = (float)SCREEN_HEIGHT - 40.0f;
-    DrawLine(0, (int)groundY, SCREEN_WIDTH, (int)groundY, DARKGRAY);
     draw_platforms(camY);
     for (int i = 0; i < MAX_AGENTS; i++)
       draw_agent_jk(&agents[i], camY);
