70f078d15a7ce51b3597c7b8932e5e010b47ac10
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 13:56:54 -0500

    Auto-save

--- utils/NN/MUZE/mcts.c
+++ utils/NN/MUZE/mcts.c
@@ -250,7 +250,6 @@ MCTSResult mcts_run_latent(MuModel *model, const float *latent,
   if (params->dirichlet_alpha > 0.0f && params->dirichlet_eps > 0.0f) {
     add_dirichlet_noise(root, params->dirichlet_alpha, params->dirichlet_eps,
                         rng);
-
     dbg_print_root("after dirichlet(root)", root);
   }
 
