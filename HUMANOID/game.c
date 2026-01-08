fbae9998b669993bd4f44246b68c9f6decb09c43
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 00:09:34 -0500

    Auto-save

--- HUMANOID/game.c
+++ HUMANOID/game.c
@@ -310,32 +310,6 @@ static void draw_agent(const Agent *a) {
     DrawCircleV(a->pt[i].p, r, RAYWHITE);
   }
 }
-static void draw_agent(Agent *a, Texture2D *ap, Texture2D *tc) {
-  // Head
-  DrawCircleV((Vector2){a->x, a->y}, 10, RAYWHITE);
-
-  // Neck
-  DrawLine(a->x, a->y, a->x, a->y - 20, RAYWHITE);
-
-  // Body
-  DrawLine(a->x, a->y - 20, a->x, a->y - 40, RAYWHITE);
-
-  // Legs
-  DrawLine(a->x, a->y - 40, a->x - 10, a->y - 60, RAYWHITE);
-  DrawLine(a->x, a->y - 40, a->x + 10, a->y - 60, RAYWHITE);
-
-  // Arms
-  DrawLine(a->x, a->y - 20, a->x - 10, a->y - 40, RAYWHITE);
-  DrawLine(a->x, a->y - 20, a->x + 10, a->y - 40, RAYWHITE);
-
-  // Feet
-  DrawLine(a->x - 10, a->y - 60, a->x - 20, a->y - 80, RAYWHITE);
-  DrawLine(a->x + 10, a->y - 60, a->x + 20, a->y - 80, RAYWHITE);
-
-  // Hands
-  DrawLine(a->x - 10, a->y - 40, a->x - 20, a->y - 60, RAYWHITE);
-  DrawLine(a->x + 10, a->y - 40, a->x + 20, a->y - 60, RAYWHITE);
-}
 
 static void update_agent(Agent *a) {}
 
