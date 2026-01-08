8c84f86e4cbe18e9eef2119a469e7015b76e37dc
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 11:44:47 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -1942,7 +1942,7 @@ static int agent_try_harvest_forward(Agent *a, Tribe *tr, float *reward) {
   }
 
   float cost = agent_harvest_cost(r->type);
-  if (r->type == RES_TREE && a->has_axe)
+  if (r->type == RES_TREE)
     cost *= 0.78f;
   if ((r->type == RES_ROCK || r->type == RES_GOLD) && a->has_pickaxe)
     cost *= 0.78f;
