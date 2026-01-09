1938a063d6c7b1c69c426769fbeea6bb861ba0c9
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-09 14:11:47 -0500

    Auto-save

--- utils/NN/MUZE/replay_buffer.h
+++ utils/NN/MUZE/replay_buffer.h
@@ -39,7 +39,6 @@ size_t rb_push_full(ReplayBuffer *rb, const float *obs, const float *pi,
 
 // Backfill z for an already-stored slot.
 void rb_set_z(ReplayBuffer *rb, size_t idx, float z);
-
 void rb_push(ReplayBuffer *rb, const float *obs, const float *pi, float z);
 void rb_push_transition(ReplayBuffer *rb, const float *obs, int action,
                         float reward, const float *next_obs, int done);
@@ -53,7 +52,6 @@ int rb_sample_transition(ReplayBuffer *rb, int batch, float *obs_batch,
 
 size_t rb_size(ReplayBuffer *rb);
 //
-
 // Backfill z for an already-stored slot.
 void rb_set_z(ReplayBuffer *rb, size_t idx, float z);
 
