56ed05245b85382bdf9e99c3c6ae59facef52ffb
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-09 13:49:44 -0500

    Auto-save

--- utils/NN/MUZE/replay_buffer.h
+++ utils/NN/MUZE/replay_buffer.h
@@ -1,26 +1,14 @@
+// replay_buffer.h
+
 #ifndef REPLAY_BUFFER_H
 #define REPLAY_BUFFER_H
 
 #include "game_env.h"
 #include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/*
-typedef struct {
-GameState *obs;      // O
-GameState *next_obs; // O
-int action;
-float reward;
-int done;
-
-// optional MuZero targets (can be filled later):
-float *pi;    // A  (policy target, e.g. from MCTS)
-float z;      // scalar return/target value
-float v_pred; // value estimate at time of storage (optional)
-} Transition;
-*/
 
 typedef struct {
   size_t capacity;
@@ -44,14 +32,24 @@ typedef struct {
 
 ReplayBuffer *rb_create(size_t capacity, int obs_dim, int action_count);
 void rb_free(ReplayBuffer *rb);
+
+/* NEW: one push that makes a slot valid for both samplers */
+size_t rb_push_full(ReplayBuffer *rb, const float *obs, const float *pi,
+                    float z, int action, float reward, const float *next_obs,
+                    int done);
+
+/* Keep old APIs, but make them safe wrappers */
 void rb_push(ReplayBuffer *rb, const float *obs, const float *pi, float z);
 void rb_push_transition(ReplayBuffer *rb, const float *obs, int action,
                         float reward, const float *next_obs, int done);
+
+int rb_sample(ReplayBuffer *rb, int batch, float *obs_batch, float *pi_batch,
+              float *z_batch);
+
 int rb_sample_transition(ReplayBuffer *rb, int batch, float *obs_batch,
                          int *a_batch, float *r_batch, float *next_obs_batch,
                          int *done_batch);
-int rb_sample(ReplayBuffer *rb, int batch, float *obs_batch, float *pi_batch,
-              float *z_batch);
+
 size_t rb_size(ReplayBuffer *rb);
 
 #ifdef __cplusplus
