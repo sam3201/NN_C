664b1f40470e6449a3363a38390d02af39ae40fa
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-09 15:57:06 -0500

    Auto-save

--- utils/TESTS/muzero_game_env_smoke_test.c
+++ utils/TESTS/muzero_game_env_smoke_test.c
@@ -53,7 +53,7 @@ typedef struct {
 // ---- Adapter: env handle type ----
 // If your game_env is a struct, use that type here.
 // If it’s an opaque pointer, use void*.
-typedef game_env_t_t Env; // <-- CHANGE if your env type is named differently
+typedef Game_env Env; // <-- CHANGE if your env type is named differently
 
 // ---- Adapter functions (map to your real ones) ----
 
