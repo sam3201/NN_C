54793d92461aa97931bd8e9d45db2ee3536e5a19
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 21:24:15 -0500

    Auto-save

--- utils/NN/MUZE/growth.c
+++ utils/NN/MUZE/growth.c
@@ -65,6 +65,8 @@ int mu_model_grow_latent(MuModel *m, int new_L) {
   m->dyn_W_count = new_L * new_L;
   m->pred_W_count = (A + 1) * new_L;
 
+  m->dyn_W_count = new_L * (new_L + 1);
+
   m->cfg.latent_dim = new_L;
 
   return 0;
