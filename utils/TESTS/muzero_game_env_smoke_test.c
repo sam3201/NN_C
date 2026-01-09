1bb4e69c3c0647cdea128bd4b79ba4cc2364f086
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-09 16:03:18 -0500

    Auto-save

--- utils/TESTS/muzero_game_env_smoke_test.c
+++ utils/TESTS/muzero_game_env_smoke_test.c
@@ -68,7 +68,7 @@ static int env_reset(Env *e, GameState *state) {
   //   int n = game_env_reset(e, out_obs, max_obs);
   //   Obs o = game_env_reset(e); memcpy(out_obs, o.data, ...)
 
-  int n = game_env_reset(state); // <-- EDIT
+  game_env_reset(state); // <-- EDIT
   return n;
 }
 
