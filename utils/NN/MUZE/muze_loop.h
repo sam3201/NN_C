456324b90edc87cd9b03b11df9a6dd4493da77d1
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 23:33:59 -0500

    Auto-save

---
+++ utils/NN/MUZE/muze_loop.h
@@ -0,0 +1,49 @@
+#ifndef MUZE_LOOP_H
+#define MUZE_LOOP_H
+
+#include "mcts.h"
+#include "muzero_model.h"
+#include "replay_buffer.h"
+#include "selfplay.h"
+#include "trainer.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+  // outer loop
+  int iterations;
+
+  // selfplay
+  int selfplay_episodes_per_iter;
+
+  // training
+  int train_calls_per_iter; // how many times to call trainer per iter
+  TrainerConfig train_cfg;
+
+  // reanalyze
+  int use_reanalyze;              // 0/1
+  int reanalyze_samples_per_iter; // number of random states to reanalyze
+  float reanalyze_gamma;          // discount for z recompute
+} MuLoopConfig;
+
+/*
+  Full MuZero-style loop:
+    for iter:
+      selfplay -> fill replay
+      optionally reanalyze -> refresh (pi,z) targets for some samples
+      train -> policy/value + dynamics/reward
+*/
+void muze_run_loop(MuModel *model, void *env_state,
+                   selfplay_env_reset_fn env_reset,
+                   selfplay_env_step_fn env_step, ReplayBuffer *rb,
+                   const MCTSParams *base_mcts_params,
+                   const SelfPlayParams *base_sp_params,
+                   const MuLoopConfig *loop_cfg, MCTSRng *rng);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
