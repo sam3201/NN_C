5831a2b5227c9111e271e0019aae3f4a1e2ecf21
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-08 20:04:48 -0500

    Auto-save

--- utils/NN/MUZE/runtime.c
+++ utils/NN/MUZE/runtime.c
@@ -86,9 +86,4 @@ int muze_select_action(MuCortex *cortex, const float *obs, size_t obs_dim,
                        float *out_pi, size_t action_count, MCTSRng *rng) {
   if (cortex->use_mcts && cortex->mcts_model) {
     MCTSResult mr = mcts_run(cortex->mcts_model, obs, cortex->mcts_params, rng)
-                        
-
-                    &cortex->mcts_params, rng) long double * *
-               latent_seq = NULL;
-    size_t seq_len = 0;
   }
