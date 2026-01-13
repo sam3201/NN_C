15124245cce02c2c51ae6825721175c782907c43
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-13 13:35:10 -0500

    Auto-save

--- utils/TESTS/muzero_chase_curses_test.c
+++ utils/TESTS/muzero_chase_curses_test.c
@@ -247,12 +247,10 @@ static void chase_env_destroy(void *state) {
 }
 
 int main(void) {
-  int headless = 1;
+  int headless = 0;
 
-  /*
   if (getenv("MUZE_HEADLESS"))
     headless = 1;
-  */
 
   if (!headless) {
     initscr();
