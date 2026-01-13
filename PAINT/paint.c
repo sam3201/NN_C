43e9fa58f9279be1138f17de4826983bebbc1896
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-13 17:49:36 -0500

    Auto-save

--- PAINT/paint.c
+++ PAINT/paint.c
@@ -1,1 +1,21 @@
-int main(void) { printf("hello world\n"); }
+#include "../utils/NN/MUZE/all.h"
+#include "../utils/NN/TRANSFORMER.h"
+#include "../utils/Raylib/src/raylib.h"
+#include <dirent.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void) {
+  printf("hello world\n");
+
+  return 0;
+}
