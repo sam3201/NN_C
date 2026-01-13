ce56cf5840577a53bc679f6fc57b2ba895575f5c
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-13 12:53:22 -0500

    Auto-save

--- JUMP/jump.c
+++ JUMP/jump.c
@@ -1223,7 +1223,8 @@ int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "--quiet") == 0 || strcmp(argv[i], "-q") == 0) {
       muze_set_verbose(0);
-    } else if (strcmp(argv[i], "--verbose") == 0 || strcmp(argv[i], "-v") == 0) {
+    } else if (strcmp(argv[i], "--verbose") == 0 ||
+               strcmp(argv[i], "-v") == 0) {
       muze_set_verbose(1);
     }
   }
