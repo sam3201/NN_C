33f36d1aaeff4fb900afac44c5ea356698929831
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-14 15:33:37 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -10375,7 +10375,7 @@ int main(int argc, char *argv[]) {
   printf("Game state initialized to: %d\n", g_state);
 
   // Initialize mouse state for UI
-  SDL_SetWindowRelativeMouseMode(g_window, SDL_TRUE);
+  SDL_SetWindowRelativeMouseMode(g_window);
   SDL_ShowCursor();
   g_mouse_locked = 0;
   SCREEN_WIDTH = GetScreenWidth();
