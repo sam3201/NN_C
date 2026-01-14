c8e564d0d06ef07e75a8be5ee108980b21400775
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-14 14:18:34 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -29,6 +29,7 @@
 #include "../utils/NN/NN/NN.h"
 #include "../utils/NN/TRANSFORMER/TRANSFORMER.h"
 #include "../utils/Raylib/src/raylib.h"
+#include "../utils/SDL3/SDL3_compat.h"
 #include <OpenGL/gl3.h>
 #include <dirent.h>
 #include <errno.h>
