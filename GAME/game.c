9d141f9b8bf536d0c4ee9c7ee5d6929b548134f8
Samuel Dasari <samueldasari@Samuels-MacBook-Air.local>
2026-01-15 19:50:38 -0500

    Auto-save

--- GAME/game.c
+++ GAME/game.c
@@ -8963,15 +8963,14 @@ static void draw_crosshair_3d(void) {
 
 static KeyboardKey poll_any_key_pressed(void) {
   KeyboardKey keys[] = {
-      KEY_A,     KEY_B,      KEY_C,         KEY_D,          KEY_E,     KEY_F,
-      KEY_G,     KEY_H,      KEY_I,         KEY_J,          KEY_K,     KEY_L,
-      KEY_M,     KEY_N,      KEY_O,         KEY_P,          KEY_Q,     KEY_R,
-      KEY_S,     KEY_T,      KEY_U,         KEY_V,          KEY_W,     KEY_X,
-      KEY_Y,     KEY_Z,      KEY_ONE,       KEY_TWO,        KEY_THREE, KEY_FOUR,
-      KEY_FIVE,  KEY_SIX,    KEY_SEVEN,     KEY_EIGHT,      KEY_NINE,  KEY_ZERO,
-      KEY_UP,    KEY_DOWN,   KEY_LEFT,      KEY_RIGHT,      KEY_SPACE, KEY_TAB,
-      KEY_ENTER, KEY_ESCAPE, KEY_BACKSPACE, KEY_LEFT_SHIFT, KEY_F5,    KEY_F6,
-      KEY_F7};
+      KEY_A,      KEY_B,         KEY_C,          KEY_D,     KEY_E,    KEY_F,
+      KEY_H,      KEY_I,         KEY_J,          KEY_K,     KEY_L,    KEY_M,
+      KEY_N,      KEY_O,         KEY_P,          KEY_Q,     KEY_R,    KEY_S,
+      KEY_T,      KEY_U,         KEY_V,          KEY_W,     KEY_X,    KEY_Y,
+      KEY_Z,      KEY_ONE,       KEY_TWO,        KEY_THREE, KEY_FOUR, KEY_FIVE,
+      KEY_SIX,    KEY_SEVEN,     KEY_EIGHT,      KEY_NINE,  KEY_ZERO, KEY_UP,
+      KEY_DOWN,   KEY_LEFT,      KEY_RIGHT,      KEY_SPACE, KEY_TAB,  KEY_ENTER,
+      KEY_ESCAPE, KEY_BACKSPACE, KEY_LEFT_SHIFT, KEY_F5,    KEY_F6,   KEY_F7};
   for (size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); i++) {
     if (IsKeyPressed(keys[i]))
       return keys[i];
